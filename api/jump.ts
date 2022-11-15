6a08dd91fef33e02b3066fc43ee036f2cfb200e6
HanaYabuki <1847196775@qq.com>
2022-11-15 17:02:46 +0800

    bugfix: Chinese Character is Not Supported

--- api/jump.ts
+++ api/jump.ts
@@ -5,7 +5,7 @@ module.exports = async (request: VercelRequest, response: VercelResponse) => {
     const { slug } = request.query;
     const result = await dbclient.db('shorturl').collection('links').findOne({ slug });
     if (result) {
-        return response.redirect(result.url);
+        return response.redirect(encodeURI(result.url));
     }
     else {
         return response.status(400).json({ msg: 'undefined slug' });
